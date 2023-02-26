7238b84d8832f26cf49d1b1c5b67fc2e2c412693
Anneysha7 <usaas23@gmail.com>
2023-02-26 23:26:13 +1100

    modifications

--- connector.py
+++ connector.py
@@ -1,10 +1,36 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template, request
+import openai
+
+openai.api_key = "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"
+
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
 def website():
     return render_template('index.html')
+
+@app.route("/", methods = ["GET", "POST"])
+def get_details():
+    if request.method == "POST":
+        poem_language = request.form.get("")
+        translation_language = request.form.get("")
+        theme = request.form.get("")
+        type = request.form.get("")
+
+        prompt = f"{poem_language} {type} based on {theme} with a translation in {translation_language}"
+
+        return prompt
+    
+    return render_template("form.html")
+
+@app.route("/")
+def generator(prompt):
+    response = openai.Completion.create(engine="text-davinci-001",
+                                        prompt=prompt,
+                                        temperature=0.6,
+                                        max_tokens=200)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import openai
 
 openai.api_key = "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"
-prompt = "Say hello world"
+
+poem_language = ""
+translation_language = ""
+type = ""
+theme = ""
+
+prompt = f"{poem_language} {type} based on {theme} with a translation in {translation_language}"
 
 response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=6)
 print(response)
