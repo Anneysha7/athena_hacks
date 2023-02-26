1cbe0c5864d95a43e0f0aae9c44f1c82997d3764
dez2003 <desiree2021325@gmail.com>
2023-02-27 00:26:51 +0800

    minor adjustments

--- connector.py
+++ connector.py
@@ -18,19 +18,29 @@ def get_details():
         theme = request.form.get("theme-search")
         # type = request.form.get("")
 
-        prompt = f"{poem_language} based on {theme} with a translation in {translation_language}"
+        prompt1 = f"{poem_language} based on {theme} with a translation in {translation_language}"
+        prompt2 = f""
 
-        return prompt
+        return prompt1
     
-    return render_template("form.html")
+    return render_template('index.html', **locals())
 
-@app.route("/")
+@app.route("/generator", methods= ["POST", "GET"])
 def generator(prompt):
     response = openai.Completion.create(engine="text-davinci-001",
                                         prompt=prompt,
                                         temperature=0.6,
                                         max_tokens=200)
     
+    if 'choices' in response:
+        if len(response['choices']) > 0:
+            answer = response['choices'][0]['text']
+        else:
+            answer = 'Wow, you beat the AI this time.'
+    else:
+        answer = 'Wow, you beat the AI this time.'
+
+    return render_template('index.html', answer)
 
 if __name__ == '__main__':
     app.run(debug=True)
